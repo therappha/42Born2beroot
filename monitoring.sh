e19392d2c3266fb96aa3abca12f87bc5cccbd22b
therappha <therappha@gmail.com>
2024-12-02 19:01:28 +0000

    walled

--- monitoring.sh
+++ monitoring.sh
@@ -19,15 +19,15 @@ CONNECTIONS=$(ss -Ht state ESTABLISHED | wc -l)
 IP=$(hostname -I | awk '{print $1}')
 MAC=$(ip link show | grep "ether" | awk '{print $2}')
 SUDOS=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
-echo "#Architeture: $ARCH"
-echo "#Physical CPU's: $PHY_CPU"
-echo "#vCPUS: $V_CPU"
-echo "#Memory Usage: ${USED_RAM}mb/${TOTAL_RAM}mb ($PERCENT_RAM% in use) "
-echo "#Disk Usage: TODO"
-echo "#CPU rate %: TODO"
-echo "#Last Boot: $LAST_BOOT"
-echo "#LVM: $LVM_USE"
-echo "#Active Conections: $CONNECTIONS"
-echo "#IPV4/MAC: $IP ($MAC)"
-echo "Total sudos: $SUDOS"
-
+wall "
+#Architeture: $ARCH
+#Physical CPU's: $PHY_CPU
+#vCPUS: $V_CPU
+#Memory Usage: ${USED_RAM}mb/${TOTAL_RAM}mb ($PERCENT_RAM% in use)
+#Disk Usage: TODO
+#Last Boot: $LAST_BOOT
+#LVM: $LVM_USE
+#Active Conections: $CONNECTIONS
+#IPV4/MAC: $IP ($MAC)
+Total sudos: $SUDOS
+"
